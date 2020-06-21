fdb24f4c1de6fc9346a5cebaabecaa001052441d
aramanenka <arramanenka@gmail.com>
2020-06-21 23:25:22 +0300

    remove unnecessary ::

--- src/util.py
+++ src/util.py
@@ -7,4 +7,4 @@ def find_flags(flag_string):
     if flag_string is None:
         return []
     reformat_flag_string = flag.dflagize(re.sub(r':.*:', '', flag_string))
-    return re.findall(r':\w+:', reformat_flag_string)
+    return re.findall(r':(\w+):', reformat_flag_string)
