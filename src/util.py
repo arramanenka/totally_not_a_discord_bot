a63776c4e26c8a043f664e933b57fc3d8d474b7d
aramanenka <arramanenka@gmail.com>
2020-06-21 23:17:32 +0300

    search by letters only instead of wildcard

--- src/util.py
+++ src/util.py
@@ -7,5 +7,4 @@ def find_flags(flag_string):
     if flag_string is None:
         return []
     reformat_flag_string = flag.dflagize(re.sub(r':.*:', '', flag_string))
-    all_flags = re.findall(r':(.*):', reformat_flag_string)
-    return all_flags
+    return re.findall(r':\w+:', reformat_flag_string)
