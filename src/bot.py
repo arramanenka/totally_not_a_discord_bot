fa1306cab580b64cc94550ca502164f873e74a7b
aramanenka <arramanenka@gmail.com>
2020-06-21 22:10:15 +0300

    detect tags in channel

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,20 @@
+import re
+
 import discord
 
 
 class TotallyNotBot(discord.Client):
     async def on_ready(self):
         print(f'{self.user} has connected to Discord!')
+
+    async def on_message(self, message):
+        for mention in message.mentions:
+            if mention.id == self.user.id:
+                await self.reply_to_direct(message)
+
+    @staticmethod
+    async def reply_to_direct(message):
+        actual_message = re.sub(r'<.*>', '', message.content).strip()
+        if actual_message == 'map':
+            response = 'wow, such empty'
+            await message.channel.send(response)
