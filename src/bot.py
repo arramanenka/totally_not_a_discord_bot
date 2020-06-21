82c7bfc0dcc5647809bef153ff950d141ed46534
aramanenka <arramanenka@gmail.com>
2020-06-22 00:55:50 +0300

    save result as csv

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,7 @@
-import json
 import re
 
 import discord
+import pycountry
 
 from src.util import find_flags
 
@@ -18,11 +18,16 @@ class TotallyNotBot(discord.Client):
     @staticmethod
     async def reply_to_direct(message):
         actual_message = re.sub(r'<.*>', '', message.content).strip()
-        if actual_message == 'map':
+        if actual_message == 'update map':
             flag_dict = dict()
             for m in message.channel.guild.members:
                 if not m.bot:
                     flags = find_flags(message.author.nick)
                     for flag in set(flags):
-                        flag_dict[flag] = flag_dict.get(flag, 0) + 1
-            await message.channel.send(json.dumps(flag_dict))
+                        country_name = pycountry.countries.get(alpha_2=flag).name
+                        flag_dict[country_name] = flag_dict.get(country_name, 0) + 1
+            with open('map.csv', mode='w+', encoding='utf-8') as csv_file:
+                csv_file.write('Name,count')
+                for key, count in flag_dict.items():
+                    csv_file.write(f'\n{key},{count}')
+            await message.channel.send('updated')
