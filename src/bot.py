d9bc23321094a1f45acc76a825b67d4166eecc9b
aramanenka <arramanenka@gmail.com>
2020-06-22 03:46:52 +0300

    proper saving

--- src/bot.py
+++ src/bot.py
@@ -9,6 +9,8 @@ from src.util import find_flags
 class TotallyNotBot(discord.Client):
     async def on_ready(self):
         print(f'{self.user} has connected to Discord!')
+        for g in self.guilds:
+            self.save_guild_member_map(g)
 
     async def on_message(self, message):
         for mention in message.mentions:
@@ -19,17 +21,8 @@ class TotallyNotBot(discord.Client):
     async def reply_to_direct(message):
         actual_message = re.sub(r'<.*>', '', message.content).strip()
         if actual_message == 'update map':
-            flag_dict = dict()
-            for m in message.channel.guild.members:
-                if not m.bot:
-                    flags = find_flags(message.author.nick)
-                    for flag in set(flags):
-                        country_name = pycountry.countries.get(alpha_2=flag).name
-                        flag_dict[country_name] = flag_dict.get(country_name, 0) + 1
-            with open('map.csv', mode='w+', encoding='utf-8') as csv_file:
-                csv_file.write('Name,count')
-                for key, count in flag_dict.items():
-                    csv_file.write(f'\n{key},{count}')
+            guild = message.channel.guild
+            TotallyNotBot.save_guild_member_map(guild)
             await message.channel.send('I am a good boy, I updated your map!',
                                        file=discord.File(filename='map.csv', fp='map.csv'))
         elif actual_message == 'get map':
@@ -37,6 +30,31 @@ class TotallyNotBot(discord.Client):
         elif actual_message == 'help':
             await message.channel.send(
                 'To update map, write \'update map\', to map that was already created, write \'get map\'. I generate '
-                'csv for https://www.datawrapper.de/maps/')
+                'csv for www.datawrapper.de/maps/')
         else:
             await message.channel.send('Command not recognized, try help')
+
+    @staticmethod
+    def save_guild_member_map(guild):
+        flag_dict = dict()
+        for m in guild.members:
+            if not m.bot:
+                flags = find_flags(m.nick)
+                for flag in set(flags):
+                    country = pycountry.countries.get(alpha_2=flag)
+                    if country is None:
+                        if flag == 'EA':
+                            country = pycountry.countries.get(alpha_2='ES')
+                        elif flag == 'CP':
+                            country = pycountry.countries.get(alpha_2='FR')
+                        else:
+                            print(flag)
+                            continue
+                    elif country.alpha_3 == 'PRI':
+                        country = pycountry.countries.get(alpha_2='US')
+                    country_name = country.alpha_3
+                    flag_dict[country_name] = flag_dict.get(country_name, 0) + 1
+        with open(f'{guild.id}.csv', mode='w+', encoding='utf-8') as csv_file:
+            csv_file.write('ISO-Code,count')
+            for key, count in flag_dict.items():
+                csv_file.write(f'\n{key},{count}')
